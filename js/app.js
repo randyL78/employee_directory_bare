7807deed62c7c4b7a212ca6e3d17511e53766350
Randy's Mac <randydavidl78@gmail.com>
2019-04-17 15:31:23 -0400

    full project commit

---
+++ js/app.js
@@ -0,0 +1,117 @@
+// ************************************************************
+//				Globals
+// ************************************************************	
+
+// store the array of employee information
+let employees = [];
+
+// store the currently selected employee for the modal to display
+let currentEmployee = 0;
+
+// adjust settings for API's url
+const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture, email, location, phone, dob &noinfo &nat=US`
+
+// the grid container for the employee cards
+const gridContainer = document.querySelector(".grid-container");
+
+// modal overlay
+const overlay = document.querySelector(".overlay");
+
+// modal container to content to
+const modalContainer = document.querySelector(".modal-content");
+
+// modal close button
+const modalClose = document.querySelector(".modal-close");
+
+
+// ************************************************************
+//				Fetch Promise
+// ************************************************************	
+fetch(urlAPI)
+  .then(res => res.json())
+  .then(res => res.results)
+  .then(displayEmployees)
+  .catch(err => console.log(err))
+
+
+// ************************************************************
+//				Functions
+// ************************************************************	
+
+function displayEmployees(employeeData) {
+
+  employees = employeeData;
+  // store the employee HTML as we create it
+  let employeeHTML = '';
+
+  // loop through each employee and create HTML markup
+  employees.forEach((employee, index) => {
+
+    // use object destructuring make our template literal cleaner
+    let { name, email, location: { city }, picture } = employee;
+
+    // template literals make this so much cleaner!!!
+    employeeHTML += `
+      <div class="card" data-index="${index}">
+        <img class="avatar" src="${picture.large}" />
+        <div class="text-container">
+          <h2 class="name">${name.first} ${name.last}</h2>
+          <p class="email">${email}</p>
+          <p class="address">${city}</p>
+        </div>
+      </div>
+    `
+  });
+
+  gridContainer.innerHTML = employeeHTML;
+}
+
+
+// adds the required information to the modal and displays it
+function displayModal(index) {
+
+  // use object destructuring make our template literal cleaner
+  let { name, dob, phone, email, location: { city, street, state, postcode }, picture } = employees[index];
+
+  let date = new Date(dob.date);
+
+  const modalHTML = `
+    <img class="avatar" src="${picture.large}" />
+    <div class="text-container">
+      <h2 class="name">${name.first} ${name.last}</h2>
+      <p class="email">${email}</p>
+      <p class="address">${city}</p>
+      <hr />
+      <p>${phone}</p>
+      <p class="address">${street}, ${state} ${postcode}</p>
+      <p>Birthday: ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+    </div>
+  `;
+
+  overlay.classList.remove("hidden");
+  modalContainer.innerHTML = modalHTML;
+
+}
+
+// ************************************************************
+//				Event listeners
+// ************************************************************	
+
+// use event bubbling to listen for a click on a card through the grid container
+gridContainer.addEventListener('click', e => {
+
+  // make sure the click is not on the gridContainer itself
+  if (e.target !== gridContainer) {
+
+    // select the card element based on it's proximity to actual element clicked
+    const card = e.target.closest(".card");
+    const index = card.getAttribute('data-index');
+
+    displayModal(index);
+  }
+})
+
+// hide the modal overlay
+modalClose.addEventListener('click', () => {
+  overlay.classList.add("hidden");
+})
